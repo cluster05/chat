66e02b7c7861c425d6e44ce53e618c88d3f3e4ec
cluster05 <ajayakumbhar1234@gmail.com>
2022-10-01 23:14:29 +0530

    database module added

---
+++ database/database.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var once sync.Once
+
+type DataSource struct {
+	MongoDB *mongo.Client
+}
+
+func InitDatabase() (*DataSource, error) {
+
+	mongodb, err := initMongoDB("mongodb://localhost:27017")
+	if err != nil {
+		return nil, err
+	}
+
+	return &DataSource{MongoDB: mongodb}, nil
+}
+
+func initMongoDB(mongoDNS string) (*mongo.Client, error) {
+
+	var instance *mongo.Client
+	var mongoerror error
+
+	once.Do(func() {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		clientOptions := options.Client().ApplyURI(mongoDNS)
+		clientOptions.SetMaxConnIdleTime(100)
+		clientOptions.SetMaxPoolSize(1000)
+		clientOptions.SetMaxConnIdleTime(4 * time.Hour)
+
+		client, err := mongo.Connect(ctx, clientOptions)
+		if err != nil {
+			instance = nil
+			mongoerror = err
+		}
+
+		if err = client.Ping(ctx, nil); err != nil {
+			instance = nil
+			mongoerror = err
+		}
+
+		instance = client
+		mongoerror = nil
+	})
+
+	return instance, mongoerror
+
+}
