55db9d8c9c9eaa24250d57da019897a0ec35c964
cluster05 <ajayakumbhar1234@gmail.com>
2022-10-02 15:43:14 +0530

    fixed socket import

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"web-chat/api/middlewere"
 	"web-chat/database"
+	"web-chat/websocket"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +19,9 @@ func InitRouter() (*gin.Engine, error) {
 
 	router := gin.New()
 	router.Use(gin.Logger(), gin.Recovery(), middlewere.CORSMiddleware())
+
+	websocket.InitSocketIO(router, datasource)
+	log.Println("[socket][init]")
 
 	HandlerSetup(router, datasource)
 	return router, nil
